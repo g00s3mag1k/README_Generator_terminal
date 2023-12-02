226b3dab2db110b22fdf6d1afd619d3255f54eb1
Brandon <beegee54321@gmail.com>
2023-12-02 15:46:00 -0800

    adding to index.js

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const fs = require('fs'); 
+
+const inquirer = require('inquirer');
+
+const generateReadMe = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const generateQuestions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'License',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Features',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'Username',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'E-mail',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: ''
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
+
 function init() {}
 
 // Function call to initialize app
+
 init();
