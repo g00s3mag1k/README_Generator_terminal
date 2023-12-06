514784edae303587e050c506cb47329d7b710a36
Brandon <beegee54321@gmail.com>
2023-12-05 23:29:44 -0800

    displayed example of application when executed

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -29,9 +29,7 @@ function renderLicenseLink(License) {
   return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.Title}
